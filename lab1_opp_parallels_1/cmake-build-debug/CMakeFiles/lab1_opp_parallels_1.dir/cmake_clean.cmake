8734bc75462c441f0e213b0716e2045d1575e2da
jul <y.trubitsyna@g.nsu.ru>
2021-02-27 00:47:44 +0700

    Latest save

---
+++ lab1_opp_parallels_1/cmake-build-debug/CMakeFiles/lab1_opp_parallels_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_opp_parallels_1.dir/main.c.o"
+  "lab1_opp_parallels_1"
+  "lab1_opp_parallels_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/lab1_opp_parallels_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
