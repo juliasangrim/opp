8d7a0a270710380f9fd56f6a77563c04f72c948e
jul <y.trubitsyna@g.nsu.ru>
2021-05-02 15:59:38 +0700

    done

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <mpi.h>
 #include <cmath>
+#include <cstring>
 
 //initial coords
 #define X0 -1
@@ -11,11 +12,17 @@
 #define Dy 2
 #define Dz 2
 
-#define Nx 5
-#define Ny 5
-#define Nz 5
+#define Nx 330
+#define Ny 330
+#define Nz 330
 
 #define a 100000
+
+#define epsilon 0.00000001
+
+double hx = (double)Dx / (Nx - 1);
+double hy = (double)Dy / (Ny - 1);
+double hz = (double)Dz / (Nz - 1);
 
 MPI_Request sendTop;
 MPI_Request sendBottom;
@@ -23,10 +30,6 @@ MPI_Request sendBottom;
 MPI_Request recvTop;
 MPI_Request recvBottom;
 
-const double hx = (double)Dx / (Nx - 1);
-const double hy = (double)Dy / (Ny - 1);
-const double hz = (double)Dz / (Nz - 1);
-#define epsilon 0,000001
 //debug
 void printMatrix(double* array) {
     for (int i = 0; i < Ny; ++i) {
@@ -81,7 +84,7 @@ double initLayers(double* layers, int offset, int partZ) {
         for (int y = 0; y < Ny; ++y) {
             for (int x = 0; x < Nx; ++x) {
                 if (x == 0 || x == Nx - 1 || y == 0 || y == Ny - 1 || (z + offset) == 0 || (z + offset) == Nz - 1)
-                    layers[z * Nx * Ny + y * Nx + x] = valuePhi(coordX(offset + x), coordY(y), coordZ(z));
+                    layers[z * Nx * Ny + y * Nx + x] = valuePhi(coordX(x), coordY(y), coordZ(z + offset));
                 else
                     layers[z * Nx * Ny + y * Nx + x] = 0;
             }
@@ -129,40 +132,68 @@ void waitBounds(int procRank, int procNum) {
     }
 }
 
-void calcCenter(const double* layer, double* buffer, int partZ, int offset) {
-    auto factor = (double)1/((double)2 / (hx * hx) + 2 / (hy * hy) + 2 / (hz * hz) + a);
+void calcCenter(const double* layer, double* buffer, double* tmpDiff, int partZ, int offset) {
+    double factor = ((double)2 / (hx * hx) + 2 / (hy * hy) + 2 / (hz * hz) + a);
+
     for (int z = 1; z < partZ - 1; ++z) {
         for (int y = 1; y < Ny - 1; ++y) {
             for (int x = 1; x < Nx - 1; ++x) {
-                double fz = (layer[(z - 1) * Nx * Ny + y * Nx + x] + layer[(z + 1) * Nx * Ny + y * Nx + x]) / (hx * hx);
+                double fz = (layer[(z - 1) * Nx * Ny + y * Nx + x] + layer[(z + 1) * Nx * Ny + y * Nx + x]) / (hz * hz);
                 double fy = (layer[z * Nx * Ny + (y - 1) * Nx + x] + layer[z * Nx * Ny + (y + 1) * Nx + x]) / (hy * hy);
                 double fx = (layer[z * Nx * Ny + y * Nx + x - 1] + layer[z * Nx * Ny + y * Nx + x + 1]) / (hx * hx);
-                buffer[z * Nx * Ny + y * Nx + x] = (fz +fx + fy - valueRo(coordX(x), coordY(y), coordZ(z + offset))) * factor;
+                double buf = (fz +fx + fy - valueRo(coordX(x), coordY(y), coordZ(z + offset))) / factor;
+                buffer[z * Nx * Ny + y * Nx + x] = buf;
+                if (*tmpDiff < fabs(buffer[z * Nx * Ny + y * Nx + x ] - layer[z * Nx * Ny + y * Nx + x ])) {
+                    *tmpDiff = fabs(buffer[z * Nx * Ny + y * Nx + x ] - layer[z * Nx * Ny + y * Nx + x ]);
+                }
             }
         }
     }
 }
 
-void calcEdges(const double* layer, double* buffer, const double* topSlice, double* bottomSlice, int partZ, int offset, int procRank, int procNum) {
-    auto factor = (double)1/((double)2 / (hx * hx) + 2 / (hy * hy) + 2 / (hz * hz) + a);
+void calcEdges(const double* layer, double* buffer, const double* topSlice, const double* bottomSlice, double* tmpDiff, int partZ, int offset, int procRank, int procNum) {
+    auto factor = ((double)2 / (hx * hx) + 2 / (hy * hy) + 2 / (hz * hz) + a);
     for (int y = 1; y < Ny - 1; ++y) {
-        for (int x = 1; x < Nx; ++x) {
+        for (int x = 1; x < Nx - 1; ++x) {
             int z = 0;
             if (procRank !=0) {
-                double fz = (topSlice[y * Nx + x] + layer[(z + 1) * Nx * Ny + y * Nx + x]) / (hx * hx);
+                double fz = (topSlice[y * Nx + x] + layer[(z + 1) * Nx * Ny + y * Nx + x]) / (hz * hz);
                 double fy = (layer[z * Nx * Ny + (y - 1) * Nx + x] + layer[z * Nx * Ny + (y + 1) * Nx + x]) / (hy * hy);
                 double fx = (layer[z * Nx * Ny + y * Nx + x - 1] + layer[z * Nx * Ny + y * Nx + x + 1]) / (hx * hx);
-                buffer[z * Nx * Ny + y * Nx + x] = (fz +fx + fy - valueRo(coordX(x), coordY(y), coordZ(z + offset))) * factor;
+                buffer[z * Nx * Ny + y * Nx + x] = (fz +fx + fy - valueRo(coordX(x), coordY(y), coordZ(z + offset))) / factor;
+            }
+            if (*tmpDiff < fabs(buffer[z * Nx * Ny + y * Nx + x ] - layer[z * Nx * Ny + y * Nx + x ])) {
+                *tmpDiff = fabs(buffer[z * Nx * Ny + y * Nx + x ] - layer[z * Nx * Ny + y * Nx + x ]);
             }
             z = partZ - 1;
             if (procRank != procNum - 1) {
                 double fz = (layer[(z - 1) * Nx * Ny + y * Nx + x] + bottomSlice[y * Nx + x]) / (hx * hx);
                 double fy = (layer[z * Nx * Ny + (y - 1) * Nx + x] + layer[z * Nx * Ny + (y + 1) * Nx + x]) / (hy * hy);
                 double fx = (layer[z * Nx * Ny + y * Nx + x - 1] + layer[z * Nx * Ny + y * Nx + x + 1]) / (hx * hx);
-                buffer[z * Nx * Ny + y * Nx + x] = (fz +fx + fy - valueRo(coordX(x), coordY(y), coordZ(z + offset))) * factor;
+                buffer[z * Nx * Ny + y * Nx + x] = (fz +fx + fy - valueRo(coordX(x), coordY(y), coordZ(z + offset))) / factor;
+            }
+
+            if (*tmpDiff < fabs(buffer[z * Nx * Ny + y * Nx + x ] - layer[z * Nx * Ny + y * Nx + x ])) {
+                *tmpDiff = fabs(buffer[z * Nx * Ny + y * Nx + x ] - layer[z * Nx * Ny + y * Nx + x ]);
             }
         }
     }
+}
+
+double getMaxDiff(const double* phi, int partZ, int offset) {
+    double diff;
+    double tDiff = 0;
+    for (int z = 0; z < partZ; ++z) {
+        for (int y = 0; y < Ny; ++y) {
+            for (int x = 0; x < Nx; ++x) {
+                if (tDiff < fabs(phi[z * Nx * Ny + y * Nx + x ] - valuePhi(coordX(x), coordY(y), coordZ(z + offset)))) {
+                        tDiff = fabs(phi[z * Nx * Ny + y * Nx + x ] - valuePhi(coordX(x), coordY(y), coordZ(z + offset)));
+                }
+            }
+        }
+    }
+    MPI_Allreduce(&tDiff, &diff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    return diff;
 }
 
 void methodJacobi() {
@@ -176,34 +207,44 @@ void methodJacobi() {
     int* offset = new int[procNum];
     getNumParts(partSizeZ, procNum);
     getOffset(offset, partSizeZ, procNum);
-    auto* layer = new double[partSizeZ[procRank] * Nx * Ny];
-    auto* bufferLayer = new double[partSizeZ[procRank] * Nx * Ny];
+    auto * layer = new double[partSizeZ[procRank] * Nx * Ny];
+    auto * bufferLayer = new double[partSizeZ[procRank] * Nx * Ny];
     auto* topSlice = new double [Nx * Ny];
     auto* bottomSlice = new double [Nx * Ny];
+
     initLayers(layer, offset[procRank], partSizeZ[procRank]);
-    printCube(layer, partSizeZ[procRank], procNum, procRank);
+    std::memcpy(bufferLayer, layer, partSizeZ[procRank] * Nx * Ny * sizeof(double));
+    //printCube(layer, partSizeZ[procRank], procNum, procRank);
     double maxDiff;
     do {
+        double tmpDiff = 0;
         sendBound(layer, topSlice, bottomSlice, procRank, procNum, partSizeZ[procRank]);
-        calcCenter(layer, bufferLayer, partSizeZ[procRank], offset[procRank]);
+        //printCube(bufferLayer, partSizeZ[procRank], procNum, procRank);
+        calcCenter(layer, bufferLayer, &tmpDiff, partSizeZ[procRank], offset[procRank]);
         waitBounds(procRank, procNum);
-        calcEdges(layer, bufferLayer, topSlice, bottomSlice, partSizeZ[procRank], offset[procRank], procRank, procNum);
-        //debug
-//        if (procRank == 3) {
-//            std::cout << "topSlice" << "\n";
-//            printMatrix(topSlice);
-//            std::cout << "bottomSlice" << "\n";
-//            printMatrix(bottomSlice);
-//        }
+        calcEdges(layer, bufferLayer, topSlice, bottomSlice, &tmpDiff, partSizeZ[procRank], offset[procRank], procRank, procNum);
+        MPI_Allreduce(&tmpDiff, &maxDiff, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+       // std::cout << maxDiff << std::endl;
+        memcpy(layer, bufferLayer, partSizeZ[procRank] * Nx * Ny * sizeof(double));
     }
-    while (maxDiff < epsilon);
+    while (maxDiff >= epsilon);
+    double delta;
+    delta = getMaxDiff(layer, partSizeZ[procRank], offset[procRank]);
+    std::cout << "Result:" << delta <<std::endl;
     delete[] partSizeZ;
-    delete[]layer;
+    delete[] layer;
+    delete[] topSlice;
+    delete[] bottomSlice;
+    delete[] bufferLayer;
+    delete[] offset;
 }
 
 int main(int argc, char* argv[]) {
     MPI_Init(&argc, &argv);
+    double start = MPI_Wtime();
     methodJacobi();
+    double end = MPI_Wtime();
+    std::cout << "Time:" << end - start << std::endl;
     MPI_Finalize();
     return 0;
 }
